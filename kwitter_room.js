82d9f29aa66040d336ad9ec114f8189aaee25367
Emmanueladeyemi123 <96444111+Emmanueladeyemi123@users.noreply.github.com>
2022-05-31 15:54:03 +0100

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBU5Pw9Gf1IvdhbrmyBg3LZ9X88ljoDH1U",
+    authDomain: "project-kwitter-cddf1.firebaseapp.com",
+    projectId: "project-kwitter-cddf1",
+    storageBucket: "project-kwitter-cddf1.appspot.com",
+    messagingSenderId: "811577753909",
+    appId: "1:811577753909:web:af8de6f4785e2a39ec4c7c"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
